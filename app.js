6e96b58781a7cb3b44a1e0ecfdd44922b2542793
unknown <alperensafak@gmail.com>
2021-03-14 11:34:17 +0300

    add post

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const connectDatabase = require("./helpers/db/connectDatabase");
 const path = require("path");
+const bodyParser = require("body-parser")
 const exphbs = require("express-handlebars");
-
 const dotenv = require("dotenv");
-
 
 dotenv.config({ path: "./config/env/config.env" });
 
-
 connectDatabase();
-
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
@@ -22,29 +19,17 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.render("site/index");
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
 
-app.get("/about", (req, res) => {
-  res.render("site/about");
-});
-
-app.get("/blog", (req, res) => {
-  res.render("site/blog");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("site/contact");
-});
-
-app.get("/login", (req, res) => {
-  res.render("site/login");
-});
-
-app.get("/register", (req, res) => {
-  res.render("site/register");
-});
+const main = require("./routes/main");
+const posts = require("./routes/posts");
+app.use("/", main);
+app.use("/posts", posts);
 
 app.listen(port, hostname, () =>
   console.log(`Example app listenin on http://${hostname}:${port}`)
