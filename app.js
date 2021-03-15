ce58144027ea1a2d57fc62c81bc597093725294e
unknown <alperensafak@gmail.com>
2021-03-15 12:10:22 +0300

    date,login,register

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const dotenv = require("dotenv");
 const fileUpload = require("express-fileupload");
+const generateDate = require("./helpers/handlebars/generateDate").generateDate;
 
 dotenv.config({ path: "./config/env/config.env" });
 
 connectDatabase();
-
-
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
@@ -21,7 +20,14 @@ app.use(fileUpload());
 
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs());
+app.engine(
+  "handlebars",
+  exphbs({
+    helpers: {
+      generateDate,
+    },
+  })
+);
 app.set("view engine", "handlebars");
 
 // parse application/x-www-form-urlencoded
@@ -30,10 +36,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+const myMiddleware = (req, res, next) => {
+  console.log("MY NAME IS ALPEREN");
+  next();
+};
+
+app.use("/", myMiddleware);
+
 const main = require("./routes/main");
 const posts = require("./routes/posts");
+const users = require("./routes/users");
 app.use("/", main);
 app.use("/posts", posts);
+app.use("/users", users);
 
 app.listen(port, hostname, () =>
   console.log(`Example app listenin on http://${hostname}:${port}`)
--- helpers/db/connectDatabase.js
+++ helpers/db/connectDatabase.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(
-      process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
     .then(() => {
       console.log("mongodb connection success");
     })
@@ -17,5 +15,4 @@ const connectDatabase = () => {
     });
 };
 
-
-module.exports=connectDatabase;
+module.exports = connectDatabase;
